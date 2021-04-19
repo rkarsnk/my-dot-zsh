f0ffadda5b5b9b3eedda6530906d54e7d4f00111
rkarsnk <git-repos-rkarsnk@rkarsnk.jp>
2021-04-19 12:47:40 +0900

    vscodeでも有効化.

--- zsh_prompt.sh
+++ zsh_prompt.sh
@@ -3,5 +3,8 @@ export LSCOLORS=ExCxcxdxBxegedabagacad
 if [[ ${TERM_PROGRAM} != "vscode" ]]; then
 	sharp='\uE0B0'
 	PROMPT=`echo %K{white}%F{red}%B%n@%m%b%f%k%F{white}%K{blue}${sharp} %k%f%K{blue}%F{white}%B%~%b %#%f%k%F{blue}${sharp} %f`
+else
+	sharp='\uE0B0'
+	PROMPT=`echo %K{white}%F{red}%B%n@%m%b%f%k%F{white}%K{blue}${sharp} %k%f%K{blue}%F{white}%B%~%b %#%f%k%F{blue}${sharp} %f`
 fi
 
