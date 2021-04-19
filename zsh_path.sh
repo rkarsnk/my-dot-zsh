720ca9b57526da35ae34e31ad11d5a1ee171f1bd
KANASHIRO Akira <ak1ra.kanash1r0@rkarsnk.jp>
2021-04-19 19:42:02 +0900

    pyenv,rbenvの設定ミスを修正.

--- zsh_path.sh
+++ zsh_path.sh
@@ -1,18 +1,17 @@
 # for pyenv
-PYENVDIR=$HOME/.pyenv/bin
-if [ -d $PYENVDIR ];then
-	export PATH=$HOME/.pyenv/bin:$PATH
+PYENVDIR="$HOME/.pyenv"
+if [ -d $PYENVDIR ]; then
 	eval "$(pyenv init -)"
 	eval "$(pyenv virtualenv-init -)"
 fi
 
 #export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
 # for rbenv
-RBENVDIR=$HOME/.rbenv/bin
-if [ -d $RBENVDIR ];then
-	export PATH=$RBENVDIR:$PATH
+RBENVDIR="$HOME/.rbenv"
+if [ -d $RBENVDIR ]; then
 	eval "$(rbenv init -)"
 fi 
+
 
 # for WSL
 VSCODEDIR=/mnt/c/Program\ Files/Microsoft\ VS\ Code/bin
